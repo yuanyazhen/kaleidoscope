6b8728c7af95c86b407efdb56cc2d51d357fa919
didi <yazhener@icloud.com>
2019-06-24 15:32:03 +0800

    feat: add 6-ZigZag-Conversion

--- leetcode/6-ZigZag-Conversion.js
+++ leetcode/6-ZigZag-Conversion.js
@@ -1,31 +1,61 @@
 /**
+ * Runtime: 228 ms
+ * Memory Usage: 69.1 MB
  * @param {string} s
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-  const res = new Array(numRows);
-  const sArr = s.split('');
+var convert = function (s, numRows) {
+  if (numRows === 1) return s;
 
-  let j, k;
-  let i = 0;
-  // for(let i = 0; i < sArr.length; ++i) {
-    j = 0;
-    k = 0;
-    while(i < sArr.length) {
-      if(j === 0) {
-        for(j = 0; j< numRows; ++j) {
-          res[j][k] = sArr[++i];
-        }
-      }
-      if(j === numRows -1) {
-        for(j; j>=0; --j) {
-          res[j][++k] = sArr[++i];          
-        }
-      }
+  // 初始化数组
+  const res = new Array(numRows);
+  for (let i = 0; i < numRows; i++) {
+    res[i] = new Array();
+  }
+
+  let len = 0;
+  let i = 0, j = 0;
+  let row = numRows - 1;
+  while (len < s.length) {
+    // 按照下标特点，计算下一个下标的位置
+    if (i === 0 || (i % row != 0 && j % row === 0)) {
+      res[i++][j] = s[len++];
+    } else if (i === row || (i % row != 0 && j % row != 0)) {
+      res[i--][j++] = s[len++];
     }
-  return res;
+  }
+  // 如果字符串里面存在逗号，这个方法就 gg 了
+  // return res.toString().replace(/,/g, '');
+  return res.reduce((pre, item) => ([...pre, ...item.filter(i => i)]), []).join('')
 };
 
-const res = convert("aa", 4)
+/**
+ * 解释：http://algorithm.books.mafengshe.com/answer/6.html
+ * Runtime: 84 ms
+ * Memory Usage: 38.1 MB
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert_1 = function (s, numRows) {
+  if (numRows === 1) return s;
+
+  let res = '';
+  // 每个 LV 结构有 2*numRows-2 个元素
+  let size = 2 * numRows - 2;
+  // 遍历每行
+  for (let i = 0; i < numRows; ++i) {
+    // 每个LV结构第一个元素下标 j；
+    for (let j = i; j < s.length; j += size) {
+      res += s[j];
+      if(i != 0 && i != numRows -1 && (size - i) + (j - i) < s.length) {
+        res += s[(size - i) + (j - i)]
+      }
+    }
+  }
+  return res;
+}
+
+const res = convert_1("PAYPALISHIRING", 3)
 console.log(res)
--- leetcode/README.md
+++ leetcode/README.md
@@ -2,6 +2,42 @@
 
 至少每天一道难度中上算法题，come on.
 
-- 2019-06-20: [3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)  
+- 2019-06-20: [3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
 - 2019-06-21: [5. Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/submissions/)
-- 2019-06-22 [6. ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion/)
+- 补：2019-06-22 [6. ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion/)
+- 补：2019-06-23 []()
+- 2019-06-24 []()
+
+> 1. http://algorithm.books.mafengshe.com/answer/6.html
+
+## 解释
+### 6. ZigZag Conversion
+> http://algorithm.books.mafengshe.com/answer/6.html  
+
+  <img src="assets/6-1.png" />
+  
+  ```bash
+  i 行遍历
+  j V 组遍历
+  ****************
+  1. 当 i=0;
+    第一行两个数据分别为 s[0], s[8];
+  2. 当 i=1;
+    j=1;
+    第一个数：push s[1];
+    第二个斜方向数：(j-1)+(size-1)
+      位置:（size-1）
+      分组的起始下标：(j-1)
+    第三个数：push s[9]
+  3. 当 i;
+    j=i;
+    第一行和最后一行直接 push s[j]；
+    第二个数：(j-i)+(size-i)
+      位置:（size-i）
+      分组的起始下标：(j-i)
+
+    j+size 循环
+  ```
+
+
+
