4c90ee93a682fe525e274b6b1ead38090ea44345
didi <yazhener@icloud.com>
2019-06-25 11:09:30 +0800

    feat: 17-Letter-Combinations-of-a-Phone-Number

--- leetcode/17-Letter-Combinations-of-a-Phone-Number.js
+++ leetcode/17-Letter-Combinations-of-a-Phone-Number.js
@@ -1,20 +1,35 @@
 /**
+ * 总算有一个跑完时间比较短的了。。
+ * Runtime: 44 ms
+ * Memory Usage: 33.9 MB
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
+var letterCombinations = function (digits) {
     const map = new Map();
     let j = 2;
-    for(let i = 97;i<=122;i) {
-        if(j === 9 || j===7) {
-            map.set('' + j++, [String.fromCharCode(i++), String.fromCharCode(i++), String.fromCharCode(i++), String.fromCharCode(i++)])
-        }else {
-            map.set('' + j++, [String.fromCharCode(i++), String.fromCharCode(i++), String.fromCharCode(i++)])
+    for (let i = 97; i <= 122; i) {
+        if (j === 9 || j === 7) {
+            map.set('' + j++, new Array(4).fill(0).map(item => String.fromCharCode(i++)))
+        } else {
+            map.set('' + j++, new Array(3).fill(0).map(item => String.fromCharCode(i++)))
         }
     }
+    // 合并成二维数组，然后从左到右两两合并
+    const digitsArr = digits.split('').map(item => map.get(item));
+    const res = digitsArr.reduce((pre, item) => {
+        if (pre.length === 0) return pre = item;
+        let temp = []
+        for (let i = 0; i < pre.length; i++) {
+            for (let j = 0; j < item.length; j++) {
+                temp.push(`${pre[i]}${item[j]}`)
+            }
+        }
+        return temp;
+    }, []);
     return res;
 };
 
-const res = letterCombinations('28');
+const res = letterCombinations('29');
 // console.log(String.fromCharCode(86));
 console.log(res);
