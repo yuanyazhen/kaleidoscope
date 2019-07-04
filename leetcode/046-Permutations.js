cb82162f9d971935bd3250bc7b1e8ba4bc5e2430
didi <yazhener@icloud.com>
2019-07-04 18:01:53 +0800

    fix: 优化 permute

--- leetcode/046-Permutations.js
+++ leetcode/046-Permutations.js
@@ -1,31 +1,35 @@
+// 046-Permutations
+
 /**
- * 递归 DFS:深度优先算法
+ * Runtime: 56 ms
+ * Memory Usage: 36.4 MB
+ * beats 99.27 %
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-    var result = [];
-    getPermute(0);
-    return result;
-
-    function getPermute(index){
-        if(index === nums.length){
-            result.push(nums.slice(0));
-            return;
-        }
-        for(var i = index; i < nums.length; i++){
-            switchNum(i, index);
-            getPermute(index + 1);
-            switchNum(i, index);
-        }
-    }
-    function switchNum(i, j){
-        if(i === j) return;
-        var tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
+const permute = function (nums) {
+    const results = [];
+    permuteDsf(nums, 0, results);
+    return results;
 };
 
-const res1 = permute([1,2,3]);
+function permuteDsf(nums, start, results) {
+    // 最后一个元素，前面已经排列好
+    if(start === nums.length - 1) results.push([...nums]);
+
+    for(let i = start; i < nums.length; ++i) {
+        // 将第一个元素分别与后面的元素进行交换
+        swap(nums, start, i)
+        permuteDsf(nums, start + 1, results)
+        swap(nums, start, i)
+    }
+}
+
+function swap(nums, index1, index2) {
+    const temp = nums[index1];
+    nums[index1] = nums[index2];
+    nums[index2] = temp;
+}
+
+const res1 = permute([1, 2, 3]);
 console.log(res1);
